5fa3c682904ab7e198273362015e530fcd192f76
Your Name <josephmoscoso70@hotmail.com>
2018-08-24 14:02:39 -0600

    clases creadas

---
+++ lab5_JosephMoscoso/src/lab5_josephmoscoso/empleados.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5_josephmoscoso;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Joseph
+ */
+public class empleados {
+
+    private String nombre;
+    private Date fecha;
+    private String user;
+    private String contra;
+    private String correo;
+    private String puesto;
+
+    public empleados() {
+    }
+
+    public empleados(String nombre, Date fecha, String user, String contra, String correo, String puesto) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.user = user;
+        this.contra = contra;
+        this.correo = correo;
+        this.puesto = puesto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    @Override
+    public String toString() {
+        return correo;
+    }
+
+}
---
+++ lab5_JosephMoscoso/src/lab5_josephmoscoso/peliculas.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5_josephmoscoso;
+
+/**
+ *
+ * @author Joseph
+ */
+public class peliculas {
+    private String nombre;
+    private String duracion;
+    private String clasificacion;
+    private String categoria;
+
+    public peliculas() {
+    }
+
+    public peliculas(String nombre, String duracion, String clasificacion, String categoria) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.clasificacion = clasificacion;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+}
